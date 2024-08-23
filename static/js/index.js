a47e71142db64fd825ef077c4a91134b33c29344
muhammad ismail <muhismailmks@gmail.com>
2024-08-24 06:52:22 +0800

    first commit

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,14 @@
-    // copy address
+// menu 
+const menu_btn = document.querySelector('.menu_btn');
+const links = document.querySelector('.links');
+
+menu_btn.addEventListener('click', () => {
+    menu_btn.classList.toggle('active')
+    links.classList.toggle('active')
+})
+ 
+ // copy address
+    const copy_address = document.querySelector(".copy_address");
     const copybtn = document.querySelector(".contractCopy");
     
      copybtn.addEventListener("click", function() { 
@@ -18,3 +28,22 @@
 
             navigator.clipboard.writeText(addressText);
         }); 
+        
+        copy_address.addEventListener("click", function() {  
+        let textSpan = copy_address.querySelector('span');
+        let message = copy_address.querySelector('.message');
+        let intervalId;
+            let addressText = `0x4334ef6f3600280A0935964799025BbaE86b4f33`
+            message.classList.add('active')
+            if (intervalId) {  
+                clearInterval(intervalId);
+            }
+
+            intervalId = setInterval(() => {
+                message.classList.remove('active')
+                clearInterval(intervalId);  
+            }, 1000);
+
+            navigator.clipboard.writeText(addressText);
+        }); 
+
