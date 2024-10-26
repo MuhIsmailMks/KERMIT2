32646fbdc6b8cf73e603656eeecef1f8e4cd01af
muhammad ismail <muhismailmks@gmail.com>
2024-10-26 11:49:05 +0800

    update

--- static/js/index.js
+++ static/js/index.js
@@ -7,21 +7,46 @@ window.addEventListener("DOMContentLoaded", () => {
  
 
 // copy address
-    const copyAddress = document.querySelector('.copy-box');
-        
-    let text = document.querySelector('.copy-box__text'); 
+const copyAddresses = document.querySelectorAll('.copy-box');
+
+copyAddresses.forEach(copyAddress => {
+    let text = copyAddress.querySelector('.copy-box__text');
     let btnText = text.textContent;
     let timeout;
 
-    copyAddress.addEventListener('click', () => { 
-        navigator.clipboard.writeText(text.textContent).then(function () {
-            text.textContent = 'Copied';
-    
-            clearTimeout(timeout);
-            timeout = setTimeout(function () {
-                text.textContent = btnText; // Kembali ke teks asli
-            }, 2000);
-        }).catch(function (err) {
-            console.error('Failed to copy text: ', err);
-        });
+    copyAddress.addEventListener('click', () => {
+        navigator.clipboard.writeText(text.textContent)
+            .then(() => {
+                text.textContent = 'Copied';
+
+                clearTimeout(timeout);
+                timeout = setTimeout(() => {
+                    text.textContent = btnText; // Kembali ke teks asli
+                }, 2000);
+            })
+            .catch(err => {
+                console.error('Failed to copy text: ', err);
+            });
     });
+});
+
+
+// menu-button
+const menuButton = document.querySelector('.menu-button');
+const menuContainer = document.querySelector('.navbar nav');
+const navContainer = document.querySelector('.menuContainer');
+const socialLinks = document.querySelectorAll('.socialLink');
+
+menuButton.addEventListener('click', () => {
+    menuButton.classList.toggle('active')
+    navContainer.classList.toggle('active')
+    menuContainer.classList.toggle('active')
+})
+
+socialLinks.forEach(socialLink => {
+    socialLink.addEventListener('click', () => {
+        menuButton.classList.remove('active')
+        navContainer.classList.remove('active')
+        menuContainer.classList.remove('active')
+    })
+})
